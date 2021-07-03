db186598a03bace54c07cd6a7dfe398a8236acef
Caique Lourenço <caique.lourenco@modalgr.com.br>
2021-07-03 15:38:34 -0300

    Created interfaces

---
+++ src/interfaces/driver.ts
@@ -0,0 +1,10 @@
+export default interface Driver {
+  id_driver: string;
+  travels: number;
+  name: string;
+  contact: string;
+  cpf: string;
+  entrance: string;
+  exit: string;
+  pin: string;
+}
---
+++ src/interfaces/travels.ts
@@ -0,0 +1,16 @@
+export default interface Travels {
+  id_travel: string;
+  created: string;
+  status: string;
+  address: string;
+  number: string;
+  description: string;
+  date: string;
+  hour: string;
+  cep: string;
+  city: string;
+  state: string;
+  id_driver: string;
+  name_driver: string;
+  pin: string;
+}
